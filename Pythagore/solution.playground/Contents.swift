8daf7b8d021b00c97e8e063716aafaea6b9ac8e5
Schmidt Thibaut <thibaut@chargemap.com>
2021-08-07 11:05:08 +0200

    Pythagore training

---
+++ Pythagore/solution.playground/Contents.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+let a: Double = 3
+let aSquare = a*a
+let b: Double = 4
+let bSquare = b*b
+let c: Double = 5
+let cSquare = c*c
+
+if aSquare + bSquare == cSquare
+{
+    print("Le triangle est rectangle.")
+}
+else
+{
+    print("Le triangle n'est pas rectangle.")
+}
+
---
+++ Pythagore/training.md
@@ -0,0 +1,3 @@
+Pour cette exercice, vous allez définir 3 variables `Double` qui représenteront les 3 côté d'un triangle.
+Si le triangle est rectangle, imprimez à l'écran (print) "Le triangle est rectangle".
+Sinon, imprimez "Le triangle n'est pas rectangle".
